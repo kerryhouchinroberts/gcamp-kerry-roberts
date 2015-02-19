e2e864117fa937fb837e31ecd83bf2804037131a
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-19 15:08:12 -0700

    Add registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
-  def full_name
-    "#{first_name} #{last_name}"
-  end
 
   has_secure_password
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
