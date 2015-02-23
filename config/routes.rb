8891bf7f52965a30a617ce344868e373e0ae1ead
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-22 23:39:18 -0700

    Fix typo in signout path

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   post '/sign-up' => 'registrations#create'
   get '/sign-in' => 'sessions#new', as: :signin
   post '/sign-in' => 'sessions#create'
-  get '/sign-out' => 'sessoions#destroy', as: :signout
+  get '/sign-out' => 'sessions#destroy', as: :signout
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
