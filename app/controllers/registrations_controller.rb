b93afc6a5787ce70eb04ee62bde588534a4d6905
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-22 23:10:44 -0700

    Update user_params in registrations controller to allow first name and last name, and redirect users to root path after creating account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id]= @user.id
-      redirect_to users_path
+      redirect_to root_path
     else
       render :new
     end
@@ -16,6 +16,6 @@ class RegistrationsController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
