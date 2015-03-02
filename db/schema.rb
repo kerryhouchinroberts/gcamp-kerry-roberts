6726a3664d6269dcc888b5d2dfc6af4f1440f6cf
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-02 13:44:51 -0700

    Create migration to have task completed valude default to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227221829) do
+ActiveRecord::Schema.define(version: 20150302203915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20150227221829) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.date     "due_date"
-    t.boolean  "completed"
+    t.boolean  "completed",   default: false
     t.integer  "project_id"
   end
 
