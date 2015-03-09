4ee82ce648e2ae28b04f1c0f47d754f97a68d2bc
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-09 17:06:59 -0600

    Add validation for unique users on memberships with custom error messaging

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,8 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  validates :user, presence: true, uniqueness: true
+  validates :user, presence: true
+  validates_uniqueness_of :user, :message => "has already been added to this project" 
 
   enum role: {member: 0, owner: 1}
 end
