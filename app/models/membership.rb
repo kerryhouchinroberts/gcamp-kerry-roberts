3df962499f67d18d0257c412eeeca53e1e1eee52
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-09 22:41:52 -0600

    Add scope on membership validation for uniqueness, so that a user can be added to multiple projects but only one time per project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,7 +3,8 @@ class Membership < ActiveRecord::Base
   belongs_to :project
 
   validates :user, presence: true
-  validates_uniqueness_of :user, :message => "has already been added to this project" 
+  validates :user, uniqueness: { scope: :project_id,
+    message: " has already been added to this project" }
 
   enum role: {member: 0, owner: 1}
 end
