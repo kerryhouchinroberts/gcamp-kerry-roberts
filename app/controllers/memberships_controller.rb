fca4980b7f229bd452b78a529fd4e36906680e07
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-11 17:24:48 -0600

    Add ability for users to update memberships on membership index page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,7 +3,7 @@ class MembershipsController < ApplicationController
   def index
     @memberships = Membership.all
     @project = Project.find(params[:project_id])
-    @membership = @project.memberships.build
+    @membership = Membership.new
   end
 
   def create
@@ -11,11 +11,29 @@ class MembershipsController < ApplicationController
     @project = Project.find(params[:project_id])
     @membership.project_id = @project.id
     if @membership.save
-      redirect_to project_path(@project), notice: "#{@membership.user.full_name} was successfully added."
+      redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was successfully added."
     else
       flash.now[:alert] = @membership.errors.full_messages
       render :index
     end
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @membership = Membership.find(params[:id])
+    if @membership.update(membership_params)
+      redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was successfully updated."
+    else
+      flash.now[:alert] = @membership.errors.full_messages
+      render :index
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    @membership = Membership.find(params[:id])
+    @membership.destroy
+    redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was successfully deleted."
   end
 
   private
