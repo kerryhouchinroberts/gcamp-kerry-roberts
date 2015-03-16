878374b2252b62f0cc8bb13c0ec86baee3a7f1bd
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-16 17:05:06 -0600

    Add dependent destroy on memberships under projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
 
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   validates :name, presence: true
