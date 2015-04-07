a085c00af1f77a1c14def5bb39dfeca13f5e83a2
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-04-07 16:34:23 -0600

    Add dependent destroy on comments under tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
 
   belongs_to :project
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :users, through: :comments
 
   validates :description, presence: true
