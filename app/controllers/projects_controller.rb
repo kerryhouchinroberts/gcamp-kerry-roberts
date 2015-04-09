0ade457176bc2b987915a11301cefb4cc4a680b3
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-04-08 22:48:30 -0600

    User is directed to task index page after creating a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,7 +23,7 @@ class ProjectsController < ApplicationController
 
     if @project.save
       Membership.create(role: 1, user_id: current_user.id, project_id: @project.id)
-      redirect_to project_path(@project), notice: 'Project was successfully created.'
+      redirect_to project_tasks_path(@project), notice: 'Project was successfully created.'
     else
       flash.now[:alert] = @project.errors.full_messages
       render :new
